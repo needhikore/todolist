01c67e3d54368434c4b3924913b5595f69bfce69
Needhi Kore <needhikore@gmail.com>
2020-05-22 22:09:10 +0530

    init commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+    return (
+        <React.fragment>
+            <h1>About</h1>
+            <p>This is TodoList app v1.0.0. It is part of a 
+                React crash course</p>
+        </React.fragment>
+    )
+}
+
+
+export default About;
